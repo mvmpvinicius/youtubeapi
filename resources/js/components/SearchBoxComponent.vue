839c779686a5fd6f8512232e348a3541088b2372
Marcus Palombello <mvmpvinicius@gmail.com>
2020-02-19 15:37:50 +0000

    Youtube video list implemented

--- resources/js/components/SearchBoxComponent.vue
+++ resources/js/components/SearchBoxComponent.vue
@@ -1,7 +1,41 @@
 <template>
     <div class="container">
-        <input type="text" v-model="search" placeholder="Searching...">
-        <button v-on:click="searchVideo">Go!</button>
+        <div class="row mb-5 mt-5">
+            <div class="col"></div>
+            <div class="col">
+                <div class="input-group">
+                    <input type="text" class="form-control" v-model="search" placeholder="Searching..." @keyup.enter="searchVideo">
+                    <div class="input-group-prepend">
+                        <button type="button" class="btn btn-dark" v-on:click="searchVideo">Go!</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col"></div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <div class="card mb-3" v-for="item in videosList">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img :src="item.snippet.thumbnails.high.url" class="card-img">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ item.snippet.title }}</h5>
+                                <p class="card-text">
+                                    <small class="text-muted">{{ item.snippet.channelTitle }}</small>
+                                </p>
+                                <p class="card-text">{{ item.snippet.description }}</p>
+                                <p class="card-text published-at">
+                                    <small class="text-muted">{{ item.snippet.publishedAt }}</small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,7 +43,8 @@
     export default {
         data () {
             return {
-                search: ''
+                search: '',
+                videosList: []
             }
         },
         mounted() {},
@@ -21,6 +56,7 @@
                 })
                 .then(res => {
                     console.log(res)
+                    this.videosList = res.data.items
                 })
                 .catch(e => {
                     console.log(e)
@@ -29,3 +65,14 @@
         }
     }
 </script>
+
+<style scoped>
+    .card-text.published-at {
+        position: absolute;
+        right: 1em;
+        bottom: 1em;
+    }
+    .card-title {
+        margin-bottom: 0em;
+    }
+</style>
